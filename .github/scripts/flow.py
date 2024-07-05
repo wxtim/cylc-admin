47713c843fbdf27c5758af5ed8ae3b7c77ff20a2
Tim Pillinger <tim.pillinger@metoffice.gov.uk>
2024-07-05 11:53:57 +0100

    Try to run a workflow using the Cylc test framework

--- .github/scripts/flow.py
+++ .github/scripts/flow.py
@@ -4,3 +4,13 @@ import pytest
 @pytest.mark.parametrize('planet', [('Earth'), ('Venus')])
 def test_foo(planet):
     print(f'Hello {planet}')
+
+
+def test_cylc_workflow(flow, scheduler, run, complete):
+    id_ = flow({
+        'scheduling': {'R1': 'foo'},
+    })
+    schd = scheduler(id_)
+    async with schd:
+        complete(schd)
+
