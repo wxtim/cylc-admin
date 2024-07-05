d1ac46df0aa7bba0c4f35a23f1e555ad375be707
Tim Pillinger <tim.pillinger@metoffice.gov.uk>
2024-07-05 12:01:31 +0100

    Try to make Cylc conftests available

--- .github/scripts/flow.py
+++ .github/scripts/flow.py
@@ -1,8 +1,11 @@
 import pytest
+import cylc
+import sys
 
 
 @pytest.mark.parametrize('planet', [('Earth'), ('Venus')])
 def test_foo(planet):
+    print(cylc.__file__, file=sys.stderr)
     print(f'Hello {planet}')
 
 
